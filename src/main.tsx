9e118b22d193c066860157cba9faecbcc3e8fddb
gstovold <glen@unionroom.com>
2026-04-29 13:37:27 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import LoadingScreen from './components/LoadingScreen'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')!
+
+// Render app immediately behind the loader
+createRoot(root).render(
   <StrictMode>
+    <LoadingScreen />
     <App />
   </StrictMode>
 )
+
+// Fade out loader after 2.2 seconds
+setTimeout(() => {
+  const loader = document.getElementById('loader')
+  if (loader) {
+    loader.classList.add('loader-hidden')
+  }
+}, 2200)
